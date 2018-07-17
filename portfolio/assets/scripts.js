5c7f8b0c6e9d8493f8130fffaeb01179172036db
Davide Becker <github@davide.me>
2018-07-17 21:04:16 +0200

    Add portfolio

---
+++ portfolio/assets/scripts.js
@@ -0,0 +1,226 @@
+var projects = [{
+    'name': 'bitvid',
+    'logo': 'bitvid.png',
+    'logoBackground': '#FFF',
+    'extraStyle': '',
+    'teaser': 'The evolution of video sharing',
+    'description': 'Mit bitvid wollten wir 2012 den damals von YouTube stark dominierten Markt mit einem Konkurenzprodukt aufmischen. Das Projekt dauerte knapp zwei Jahre an und hat einiges an Aufmerksamkeit auf sich gezogen. Nach Ende des Projektes gab es starke Anzeichen, dass Amazon unser Projekt verfolgt hat, welche das Livestreaming Portal twitch.tv kurze Zeit später für über eine Milliarde Dollar aufgekauft haben.',
+    'gallery': ['bitvid-gallery-3.png', 'bitvid-gallery-2.png',
+        'bitvid-gallery-1.png',
+
+    ],
+    links: [{
+        url: 'https://archive.davide.me/bitvid/',
+        title: 'Archivierte Version der Homepage'
+    }]
+}, {
+    'name': 'Pokemon Picross Solutions',
+    'logo': 'pokemon-picross.png',
+    'logoBackground': '#FFF',
+    'extraStyle': '',
+    'teaser': '& ähnliche Tools',
+    'description': 'Stellvertretend für einige Tools, die ich im Laufe der Jahre erstellt habe. Ist bei Google Suchen sehr weit vorne und gibt durch AdSense ein nettes passives Einkommen. Seit der Veröffentlichung der Seite Anfang 2016 knapp 2 Millionen Seitenaufrufe.',
+    'gallery': ['picross-1.png'],
+    'links': [{
+        url: 'https://apps.keeky.net/pokemon/picross/',
+        title: 'Link zur Seite'
+    }]
+}, {
+    'name': 'Bulky',
+    'logo': 'bulky.png',
+    'logoBackground': '#4E536E',
+    'extraStyle': '',
+    'teaser': 'Batch processing made simple',
+    'description': 'Ich fotografiere sehr viel und sehr gerne. Bei unserer Werkschau sind um die 1000 Bilder entstanden. Da das nicht das erste mal war, dass ich eine große Anzahl an Bildern verschicken musste habe ich ein Tool geschrieben, das das Problem löst und per Drag & Drop Bilder auf eine teilbare Größe bringt. Im Test wurde die Dateigröße von 6 GB auf 170 MB reduziert.',
+    'gallery': ['bulky-1.png'],
+    links: [{
+        url: 'https://github.com/DavideBecker/Bulky',
+        title: 'Source Code auf GitHub'
+    }]
+}, {
+    'name': 'Glace Frame',
+    'logo': 'glace-frame.png',
+    'logoBackground': '#2C3E50',
+    'extraStyle': '',
+    'teaser': 'Rapid prototyping',
+    'description': 'Glace hieß die Designsprache, die wir innerhalb von bitvid aufgebaut haben und Frame sollte ursprünglich für Rapid Prototyping von Layouts verwendet werden. Nach dem Ende von Bitvid leider nicht mehr weiterverfolgt.',
+    'gallery': [],
+    'links': [{
+        url: 'https://lab.davide.me/glace/frame/',
+        title: 'Link zur Demo Seite'
+    }]
+}, {
+    'name': 'holiday cat',
+    'logo': 'holiday-cat.jpg',
+    'logoBackground': '#000',
+    'extraStyle': '',
+    'teaser': 'Videoproduktion',
+    'description': 'Das VBT - Video Battle Turnier - ist ein Rap Turnier bei dem Wert auf Videoproduktion gelegt wird. Mit holiday cat haben wir ein Image etabliert, was sich bisher gut durchsetzt. Das Turnier dauert noch an, aber aktuell sind wir im 32stel Finale von etwa 2000 Teilnehmern. Die Vides selbst entstehen meistens komplett an einem Tag, weshalb sie qualitativ sicher nicht am hochwertigsten wirken. Für die Zeit sind sie allerdings mehr als in Ordnung.',
+    'gallery': ['holiday-cat-1.png', 'holiday-cat-2.png', 'holiday-cat-3.png'],
+    'links': [{
+        url: 'https://www.youtube.com/channel/UCfHt81aGlzEHpvoV6_eVZUg',
+        title: 'Link zum YouTube Kanal'
+    }]
+}, {
+    'name': 'Unicorn Education',
+    'logo': 'unicorn.svg',
+    'logoBackground': '#FFF',
+    'extraStyle': '',
+    'teaser': 'The new way to learn',
+    'description': 'Unicorn Education ist im Studium entstanden und noch mitten in der Entwicklung. Wir haben gemerkt, dass viele der Inhalte, die wir im Studium lernen zwar unglaublich intereesant sind, aber nirgendwo zum weiteren Beschäftigen hochwertig aufbereitet sind. Diese Lücke möchten wir selbst schließen.',
+    'gallery': ['unicorn-1.png'],
+    links: []
+}, {
+    'name': 'Pflegender Versicherungsmakler',
+    'logo': 'pflegender-versicherungsmakler.png',
+    'logoBackground': '#FFF',
+    'extraStyle': 'padding-bottom: 0',
+    'teaser': 'Absicherung von Pflegepersonal',
+    'description': 'Viele im Pfegepersonal sind schlecht abgesichert, was Risiko und Zukunft angeht. Mit dem "Pflegenden Versicherungsmakler" sollte eine Marke aufgebaut werden, die für eine faire, kundennahe Beratung steht und genau auf das Fachpersonal der Pflege spezialisiert ist. Hierfür wurde eine Webseite umgesetzt und eine Social Media Strategie erarbeitet. Vom Kunden allerdings leider nicht weiter verfolgt.',
+    'gallery': ['pflegender-versicherungsmakler-1.png', 'pflegender-versicherungsmakler-2.png'],
+    'links': [{
+        url: 'http://pflegender-versicherungsmakler.de/',
+        title: 'Link zur Seite'
+    }]
+}, {
+    'name': 'Hausbar',
+    'logo': 'hausbar.png',
+    'logoBackground': '#681414',
+    'extraStyle': '',
+    'teaser': 'Die andere Gaststätte',
+    'description': 'Die Hausbar ist eine lokale Gaststätte, die auf Regionalität und Bio setzt. Bei der Umsetzung wurde Wert darauf gelegt, dass diese Ansätze im Vordergrund stehen und sich auch im Ambiente wiederspiegelt. Es wurde eine Webseite umgesetzt, Bilder für Marketing etc aufgenommen, eine Kampagne auf Facebook geplant und die Einrichtung überarbeitet.',
+    'gallery': ['hausbar-1.png', 'hausbar-2.png'],
+    'links': []
+}, {
+    'name': 'Fresh and Local',
+    'logo': 'frocal.svg',
+    'logoBackground': '#FFF',
+    'extraStyle': '',
+    'teaser': 'Semesterprojekt 4. Semester',
+    'description': 'Das 4. Semester hat sich rund um das Thema Internet der Dinge und Vernetzung gedreht. Hier ist in unserer Recherche schnell heruasgekommen, dass es für Landwirte eine große Chance ist den Direktvertrieb zu nutzen, allerdings ist hier für viele der Aufwand zum Umstieg zu hoch. Unser Konzept gibt ihnen einen autonomen Hofladen an die Hand, der ihnen den Aufwand abnimmt.',
+    'gallery': ['frocal-1.jpg', 'frocal-2.jpeg'],
+    'links': [{
+        url: 'https://imd.mediencampus.h-da.de/projekt/fresh-and-local/',
+        title: 'Blog des Studiengangs'
+    }, {
+        url: 'https://github.com/DavideBecker/Frocal/',
+        title: 'Source Code auf GitHub'
+    }]
+}, {
+    'name': 'Essentia',
+    'logo': 'essentia.png',
+    'logoBackground': '#FFF',
+    'extraStyle': '',
+    'teaser': 'Semesterprojekt 3. Semester',
+    'description': 'Im 3. Semester haben wir uns mit dem Thema Landflucht beschäftigt und wie man dagegen vorgehen kann. Unser Ansatz bestand aus einem Lieferservice, der Dörfer ohne Supermarkt beliefert.',
+    'gallery': [],
+    'links': [{
+        url: 'https://imd.mediencampus.h-da.de/projekt/essentia/',
+        title: 'Blog des Studiengangs'
+    }, {
+        url: 'https://github.com/DavideBecker/Essentia/',
+        title: 'Source Code auf GitHub'
+    }]
+}, {
+    'name': 'e-motion',
+    'logo': 'e-motion.png',
+    'logoBackground': '#7F7F7F',
+    'extraStyle': '',
+    'teaser': 'Semesterprojekt 2. Semester',
+    'description': 'Im 2. Semester ging es um Simulationen. Meine Gruppe hat sich zum Ziel gesetzt die Elektromobilität in Stuttgart zu verbessern. Ganz besonders für Pendler war dies eine attraktive Möglichkeit bis auf die Tatsache, dass die Reichweite der Elektroautos sehr begrenzt war. Das Projekt hat die Möglichkeit erforscht mobile Elektrotankstellen in Form von Trucks einzusetzen.',
+    'gallery': ['e-motion-1.png', 'e-motion-3.png', 'e-motion-2.png'],
+    'links': [{
+        url: 'https://imd.mediencampus.h-da.de/projekt/e-motion/',
+        title: 'Blog des Studiengangs'
+    }, {
+        url: 'https://davidebecker.github.io/E-Motion/',
+        title: 'Demo'
+    }, {
+        url: 'https://github.com/DavideBecker/E-Motion/',
+        title: 'Source Code auf GitHub'
+    }]
+}, {
+    'name': 'Physics',
+    'logo': 'physics.png',
+    'logoBackground': '#FFF',
+    'extraStyle': '',
+    'teaser': 'Semesterprojekt 1. Semester',
+    'description': 'Im 1. Semester meines Studium hatten wir die Aufgabe ein Spiel zu erstellen. Wir hatten die Wahl zwischen Pong, Snake oder Memory. Ich habe mich dazu entschieden eine ganze Physik Engine zu erstellen, die flexibel genug ist, um alle diese Spiele umzusetzen. Inklusive Formen, Kollisionen, Kamera, Sprites, Maps etc.',
+    'gallery': [],
+    'links': [{
+        url: 'https://davidebecker.github.io/Physics/app/demo/',
+        title: 'Demo'
+    }, {
+        url: 'https://github.com/DavideBecker/Physics/',
+        title: 'Source Code auf GitHub'
+    }]
+}]
+
+function get(id) {
+    return document.getElementById(id)
+}
+
+function tpl(id) {
+    return doT.template(get('tpl-' + id).innerHTML)
+}
+
+var elems = {
+    projects: get('projects'),
+    back: get('back'),
+    floating: get('floating'),
+    floatingContainer: get('floating-container'),
+
+    tpl: {
+        project: tpl('project'),
+        projectDetails: tpl('project-details')
+    }
+}
+
+elems.back.addEventListener('click', function() {
+    document.body.classList.remove('floating-active')
+    document.body.classList.remove('floating-done')
+    elems.floating.innerHTML = ''
+    window.scrollTo(0, prevScroll)
+})
+
+var prevScroll
+
+for (var i in projects) {
+    var project = projects[i]
+
+    project.elem = document.createElement('div')
+    project.elem.classList.add('project')
+    project.elem.style.backgroundColor = project.logoBackground
+    project.elem.innerHTML += elems.tpl.project(project)
+    project.elem.dataset.projectKey = i
+
+    project.elem.addEventListener('click', function() {
+        var proj = projects[this.dataset.projectKey]
+        var pos = proj.elem.getBoundingClientRect()
+        var cont = elems.floatingContainer.getBoundingClientRect()
+
+        elems.floating.innerHTML = elems.tpl.projectDetails(proj)
+        elems.floating.style.top = pos.y + 'px'
+        elems.floating.style.left = (pos.x - cont.x) + 'px'
+        elems.floating.style.width = pos.width + 'px'
+        elems.floating.style.height = pos.height + 'px'
+
+        prevScroll = window.scrollY
+
+        window.setTimeout(function() {
+            document.body.classList.add('floating-active')
+            elems.floating.style.top = 0
+            elems.floating.style.left = 0
+            elems.floating.style.width = '100%'
+            elems.floating.style.height = '100%'
+
+            window.setTimeout(function() {
+                document.body.classList.add(
+                    'floating-done')
+                window.scrollTo(0, 0)
+            }, 300)
+        }, 1)
+    })
+
+    elems.projects.appendChild(project.elem)
+}
